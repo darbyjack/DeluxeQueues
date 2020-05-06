0fe409cc98c474e4aad688b2ecfb938e9366698f
Alex Wood <alexljwood24@hotmail.co.uk>
2020-05-06 17:26:20 +0100

    Add String.color return type

--- src/main/kotlin/me/glaremasters/deluxequeues/utils/Strings.kt
+++ src/main/kotlin/me/glaremasters/deluxequeues/utils/Strings.kt
@@ -3,4 +3,4 @@ package me.glaremasters.deluxequeues.utils
 import net.md_5.bungee.api.ChatColor
 
 
-fun String.color() = ChatColor.translateAlternateColorCodes('&', this)
+fun String.color(): String = ChatColor.translateAlternateColorCodes('&', this)
